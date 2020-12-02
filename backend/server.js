68b933b985e97ec42bbecce5d395c32652ec1545
ozkilic <yunusemreozkilic@outlook.com>
2020-12-02 17:53:38 +0200

    bug fixed

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.get('/api/products', (req, res) => {
 
 app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id)
-  res.json(products)
+  res.json(product)
 })
 
 const PORT = process.env.PORT || 5000
