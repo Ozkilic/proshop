a7c74378e601059416001a1a1a17bb75ffbc6a79
ozkilic <yunusemreozkilic@outlook.com>
2020-12-03 11:07:46 +0200

    install colors for logging

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 import products from './data/products.js'
+import colors from 'colors'
 
 dotenv.config()
+connectDB()
 
 const app = express()
 
@@ -23,5 +26,7 @@ const PORT = process.env.PORT || 5000
 
 app.listen(
   5000,
-  console.log(`server running in ${process.env.NODE_ENV} on port 5000 ${PORT}`)
+  console.log(
+    `server running in ${process.env.NODE_ENV} on port 5000 ${PORT}`.yellow.bold
+  )
 )
