227bdf1b527c9b59035d2324226986edacf4bd44
ozkilic <yunusemreozkilic@outlook.com>
2020-12-10 14:19:04 +0200

    user registirations and  encrypt the password

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -6,14 +6,14 @@ const protect = asyncHandler(async (req, res, next) => {
   let token
   if (
     req.headers.authorization &&
-    req.headers.authorization.startswith('Bearer')
+    req.headers.authorization.startsWith('Bearer')
   ) {
     try {
       token = req.headers.authorization.split(' ')[1]
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-      req.user = await (await User.findById(decoded.id)).select('-password')
+      req.user = await await User.findById(decoded.id).select('-password')
 
       next()
     } catch (error) {
